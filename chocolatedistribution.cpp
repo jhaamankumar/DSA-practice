b1f598290f46a027969f194e920c3d438add851e
Aman kumar jha <81789772+jhaamankumar@users.noreply.github.com>
2023-01-18 21:17:31 +0530

    Add files via upload

---
+++ chocolatedistribution.cpp
@@ -0,0 +1,15 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    long long  ans=INT_MAX;
+    sort(a.begin(),a.end());
+    int res=0;
+
+    for(int i=0;i+m-1<n;i++)
+    {
+        ans=min((a[i+m-1]-a[i]),ans);
+    }
+   return  ans;
+
+    }
