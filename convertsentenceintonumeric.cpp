5634302d502178da22af717afd4782849e82a3f5
Aman kumar jha <81789772+jhaamankumar@users.noreply.github.com>
2023-01-28 22:41:43 +0530

    Add files via upload

---
+++ convertsentenceintonumeric.cpp
@@ -0,0 +1,23 @@
+
+string printSequence(string arr[], string input)
+{
+    string output = "";
+
+    // length of input string
+    int n = input.length();
+    for (int i = 0; i < n; i++) {
+        // Checking for space
+        if (input[i] == ' ')
+            output = output + "0";
+
+        else {
+            // Calculating index for each
+            // character
+            int position = input[i] - 'A';
+            output = output + arr[position];
+        }
+    }
+
+    // Output sequence
+    return output;
+}
