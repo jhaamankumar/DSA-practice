ee0dd9bdb3974a351188478012cb06789f82daa5
Aman kumar jha <81789772+jhaamankumar@users.noreply.github.com>
2023-03-06 21:18:46 +0530

    Add files via upload

---
+++ climbing_stairs.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    int climbStairs(int n) {
+        if(n<0)
+        return 0;
+        if(n==0)
+        return 1;
+        
+        int ans=climbStairs(n-1)+climbStairs(n-2);
+        return ans;
+        
+    }
+    int main(){
+        int n;
+        cout<<"Enter the value of n"<<endl;
+        cin>>n;
+        cout<<climbStairs(n);
+        return 0;
+    }
+    
+    
